5d541d980ff894c385c10bbbe2f76bb50dbe41f2
Ayman Rehman <aimanshahzad295@gmail.com>
2024-03-31 00:29:17 +0500

    created number guessing game cli based.

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /usr/bon/env node
+
+import inquirer from "inquirer"
+
+const random_number =Math.floor(Math.random()*1010);
+
+
+const answers  =await inquirer.prompt ([
+   {
+     name:"userguessnumber",
+     type: "number",
+     message:"guess a number between 0-10",
+   }
+]);
+if ( answers.userguessnumber === random_number){
+    console.log ("congratulation ! your guess is correct.");
+}else{
+    console.log("Ohno! it's wrong");
+}
